5b7b9edc817830348227e8c7f9178d26ecc9991f
Marco Westerhof <m.westerhof@lukkien.com>
2015-07-24 17:47:37 +0200

    version 0.0.2 => pypi

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     author_email='mephistolomaniac@gmail.com',
     url='https://github.com/mephizzle/python-funkyfunc',
     name='FunkyFunk',
-    version='0.0.1-dev',
-    packages=['funkyfunc',],
+    version='0.0.2-dev',
+    packages=['funkyfunc'],
     license='Apache 2.0',
     long_description=open('README.txt').read(),
 )
